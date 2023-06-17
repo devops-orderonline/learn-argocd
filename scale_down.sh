5401b1e5ae1b9ca835075d68c84d628b98cb9dec
OO MBP 014 <oombp014@OOs-MacBook-Pro.local>
2023-06-17 15:59:23 +0700

    update

--- scale_down.sh
+++ scale_down.sh
@@ -3,7 +3,16 @@
 up=$(uptime)
 echo "Server uptime is $up"
 
+git config --global user.name 'devops orderonline'
+git config --global user.email 'devops@orderonline.id'
+
 for file in $(find . -name values.yaml)
 do
   yq e -i '.replicaCount=0' $file
+  git add $file
 done
+
+git commit -m "Scheduler Scale Down"
+git push
+
+echo "Git push has successfully"
